1551728e16c61d8b57071d4706b52a1536b6d5e5
Onakunle Oladipo Oluwaseun <23644736+diposs@users.noreply.github.com>
2024-03-23 21:42:24 +0100

    Add files via upload

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -28,13 +28,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${FrameImageUrls.START}" />
     <meta property="fc:frame:input:text" content="Input" />
-    <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
-    <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Blog post Tutorial" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Video Tutorial" />
-    <meta property="fc:frame:button:3:action" content="post_redirect" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+    <meta property="fc:frame:button:1" content="Create wallet" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/wallet" />
   </head></html>`)}
   else {
   return new NextResponse(`<!DOCTYPE html><html><head>
