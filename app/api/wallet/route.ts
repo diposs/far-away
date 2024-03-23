021cc6ecf433645745a75e0710356095cf4a00d3
Onakunle Oladipo Oluwaseun <23644736+diposs@users.noreply.github.com>
2024-03-24 00:47:09 +0100

    Update route.ts

--- app/api/wallet/route.ts
+++ app/api/wallet/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: NextRequest): Promise<Response> {
         return new NextResponse(errorFrame);
     }
     const {fid, isValid} = await parseFrameRequest(frameRequest);
-    if (!fid || !isValid) return new NextResponse(errorFrame);
+    // if (!fid || !isValid) return new NextResponse(errorFrame);
     const inputText = frameRequest.untrustedData.inputText;
     const isValidEmail = frameRequest.untrustedData.inputText
     ? /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputText)
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest): Promise<Response> {
 
     // Query FC Registry contract to get owner address from fid
     const ownerAddress = await getOwnerAddressFromFid(fid);
-    if (!ownerAddress) return new NextResponse(errorFrame);
+    // if (!ownerAddress) return new NextResponse(errorFrame);
 
     // Generate an embedded wallet associated with the fid
     if (isValidEmail && fid) {
