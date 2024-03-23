966044caa8871dcbb97605245295dfd9dd69828b
Onakunle Oladipo Oluwaseun <23644736+diposs@users.noreply.github.com>
2024-03-23 23:51:50 +0100

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,7 +5,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/7.png" />
-    <meta property="fc:frame:input:text" content="Enter your email to generate a wallet. The wallet will also be associated with your Farcaster ID." />
+    <meta property="fc:frame:input:text" content="Enter your email to generate a wallet." />
     <meta property="fc:frame:button:1" content="Sign-in/Sign-up" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/wallet" />
   </head></html>`);
