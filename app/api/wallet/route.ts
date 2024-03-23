c81431b2c313ef2a74c0e6c1817c0a3cee4120c6
Onakunle Oladipo Oluwaseun <23644736+diposs@users.noreply.github.com>
2024-03-24 00:25:25 +0100

    Update route.ts

--- app/api/wallet/route.ts
+++ app/api/wallet/route.ts
@@ -18,21 +18,24 @@ export async function POST(req: NextRequest): Promise<Response> {
     }
     const {fid, isValid} = await parseFrameRequest(frameRequest);
     if (!fid || !isValid) return new NextResponse(errorFrame);
+    const isValidEmail = frameRequest.untrustedData.inputText
+    ? /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputText)
+    : false;
 
     // Query FC Registry contract to get owner address from fid
     const ownerAddress = await getOwnerAddressFromFid(fid);
     if (!ownerAddress) return new NextResponse(errorFrame);
 
     // Generate an embedded wallet associated with the fid
-    // if (isValidEmail && fid) {
-    // try {
-      // newWallets = await createEmbeddedWallet(inputText, fid, [
-        // ChainEnum.Evm
-      // ]);
-    // } catch (e) {
-      // return new NextResponse(errorFrame);
-    // }
-  // }
+    if (isValidEmail && fid) {
+    try {
+      newWallets = await createEmbeddedWallet(inputText, fid, [
+         ChainEnum.Evm
+       ]);
+     } catch (e) {
+       return new NextResponse(errorFrame);
+     }
+   }
     const embeddedWalletAddress = await createOrFindEmbeddedWalletForFid(fid, ownerAddress);
     if (!embeddedWalletAddress) return new NextResponse(errorFrame);
 
