189495c7b37f4974447105a1a547acecea94c79d
Onakunle Oladipo Oluwaseun <23644736+diposs@users.noreply.github.com>
2024-03-24 12:51:18 +0100

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -4,7 +4,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVApz4Co3JDcAFyg9aCtasfK7bgCXsM9UyeFhdo8xT7z4" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmRNhJELpP4qgmGjMsxAc1wHZRwsinWsrdvGqEAsAg17yD" />
     <meta property="fc:frame:input:text" content="Enter your email." />
     <meta property="fc:frame:button:1" content="Sign-in/Sign-up" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/wallet" />
