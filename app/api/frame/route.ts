322f08e31807d992ab2ba6d05af92b96264d4c56
Onakunle Oladipo Oluwaseun <23644736+diposs@users.noreply.github.com>
2024-03-24 01:41:19 +0100

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 import {FrameImageUrls} from '@/lib/farcaster';
-export async function POST(req: NextRequest): Promise<NextResponse> {
+async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/7.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVApz4Co3JDcAFyg9aCtasfK7bgCXsM9UyeFhdo8xT7z4" />
     <meta property="fc:frame:input:text" content="Enter your email." />
     <meta property="fc:frame:button:1" content="Sign-in/Sign-up" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/wallet" />
   </head></html>`);
 }
 
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
 
 export const dynamic = 'force-dynamic';
